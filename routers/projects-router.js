712ede7c2285323e99d6f7b0faa4514ba5085ccc
Natalia <nataliamirandac@hotmail.com>
2020-08-07 12:31:57 -0400

    Delete - remove - getProjectsActions(id) - works

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -50,15 +50,59 @@ router.put("/:id", (req, res) => {
   const { id } = req.params;
   const updatedP = req.body;
 
-  projectModel.update(id, updatedP).then((item) => {
-    if (item.id == id) {
-      res.status(201).json({ ...updatedP });
-    } else {
+  projectModel
+    .update(id, updatedP)
+    .then((item) => {
+      if (item.id == id) {
+        res.status(201).json({ ...updatedP });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the speciefied ID does not exist" });
+      }
+    })
+    .catch((error) => {
+      console.log(error, "catch error to Update the project");
       res
         .status(500)
-        .json({ error: "null. The id was not found - status 500" });
-    }
-  });
+        .json({ error: "The post informationd could not be modified." });
+    });
+});
+
+//REMOVE - Delete - project
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  projectModel
+    .remove(id)
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch((error) => {
+      console.log("catch error - DELETE project", error);
+      res.status(500).json({
+        errormessage: "Error to DELETE a project - status 500",
+      });
+    });
+});
+
+//GET- getProjectActions(id)
+
+router.get("/:id", function (req, res) {
+  const { id } = req.params;
+
+  projectModel
+    .getProjectActions(id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log("CATCH ERROR  getProjectActions(id):", error);
+      res.status(500).json({
+        errorMessage:
+          "The projects actions information could not be retrieved.",
+      });
+    });
 });
 
 module.exports = router;
